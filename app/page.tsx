7e80718a9867667e08777219069e8018c6fbb3bf
GG202043 <qwert2340123123@gmail.com>
2024-06-20 11:38:06 +0900

    세부 수정

--- app/page.tsx
+++ app/page.tsx
@@ -51,21 +51,21 @@ export default function Home() {
     3d grapic designer
   </h1>
   <p className="max-w-[6000px] text-gray-300 md:text-xl dark:text-gray-400">
-  this is aportfolio of 3d game grapic background designer lee chan woo who makes interesing background 3d model.
+  this is a portfolio of 3d game grapic background designer lee chan woo who makes interesing background 3d model.
   </p>
 </div>
 
         </div>
         <div className="flex flex-col gap-2 min-[400px]:flex-row justify-center">
           <Link
-            className="inline-flex h-10 items-center justify-center rounded-md bg-[#6366F1] px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-[#4F46E5] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#4F46E5] disabled:pointer-events-none disabled:opacity-50"
+            className="inline-flex h-10 items-center justify-center rounded-md bg-[#6366F1] px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-[#4F46E5] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#4F46E5] disabled:pointer-events-none disabled:opacity-50 active:bg-[#4F46E5]"
             href="#"
           >
             View Projects
           </Link>
           <Link
-  className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300 dark:border-gray-800 text-black
-  active:bg-gray-300"  // 클릭 시 배경색 회색으로 변경
+  className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-300 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-200 dark:focus-visible:ring-gray-700 dark:border-gray-800 text-black
+  active:bg-gray-500"  // 클릭 시 배경색 회색으로 변경
   href="#"
 >
   Contact Me
