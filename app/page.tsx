98a69c5a24b8cbc19c6041d1f08e3357c5712a8a
GG202043 <qwert2340123123@gmail.com>
2024-06-25 10:53:27 +0900

    수정

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
 </header>
 
     {/*맨 위 머리부분*/}
-    <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 bg-gray-900 text-white flex items-center justify-center">
+    <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 bg-gray-900 text-white flex items-center justify-center" id="main">
   <div className="container px-4 md:px-6 text-center"> {/* text-center 클래스 추가 */}
     <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
       <div className="flex flex-col justify-center space-y-4">
@@ -229,7 +229,7 @@ export default function Home() {
 
   function PaletteIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
     return (
-      <svg
+      <svg id="main"
         {...props}
         xmlns="http://www.w3.org/2000/svg"
         width="24"
